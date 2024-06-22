f0f0594f3fe2112a380dd6b49106fed4f8f07bb3
1657125180@qq.com <1657125180@qq.com>
2024-06-22 16:35:43 +0800

    feat:test framework

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
+#include"Test.h"
+#include<vector>
 
 int main()
 {
-
+	int n = 1000000;
+	vector<TestCase> set;
+	for (int i = 0; i < n; i++)
+	{
+		set.emplace_back();
+	}
+	TestLauncher launcher(set, Solution::CreateDefaultSolution);
+	double ms = launcher.TimeTest();
+	cout << ms << endl;
 }
--- Test/Test.h
+++ Test/Test.h
@@ -1,8 +1,8 @@
 #pragma once
-#include "Test.h"
 #include<vector>
 #include<iostream>
 #include<chrono>
+#include<functional>
 using namespace std;
 using namespace std::chrono;
 using namespace std::literals::chrono_literals;
@@ -47,27 +47,78 @@ public:
 	}
 	long long NanoSeconds() const
 	{
+		time_point<steady_clock> temp = high_resolution_clock::now();
+		if (!paused)
+			return nanoseconds + (temp - timePoint).count();
 		return nanoseconds;
 	}
 	double Milliseconds() const
 	{
-		return nanoseconds / 1e6;
+		return NanoSeconds() / 1e6;
 	}
 };
 
-class TestCase
+//Base class,different problems have different test cases
+struct TestCase
 {
 
 };
 
+//One Solution is used for solving one TestCase
 class Solution
 {
 public:
-	vector<TestCase> set;
-	Solution(vector<TestCase>& set) :set(set)
+	static Solution CreateDefaultSolution()
+	{
+		return Solution();
+	}
+	double TimeTest(TestCase t)
+	{
+		StopWatch timer;
+		timer.Start();
+		Run(t);
+		timer.Pause();
+		return timer.Milliseconds();
+	}
+	virtual bool CorrectnessTest(TestCase t)
+	{
+		return false;
+	}
+	virtual void Run(TestCase t)
+	{
+
+	}
+};
+
+//One TestLauncher is used for measuring time and correctness of a set of Testcases
+class TestLauncher
+{
+	const vector<TestCase>& set;
+	function<Solution()> CreateSolution;
+public:
+	TestLauncher(const vector<TestCase>& set, const function<Solution()>& CreateSolution)
+		:set(set), CreateSolution(CreateSolution)
 	{
 		
 	}
-	virtual int TimeTest() = 0;
-	virtual float CorrectnessTest() = 0;
+	double TimeTest()
+	{
+		double sum = 0.0;
+		for (int i = 0; i < set.size(); i++)
+		{
+			Solution s = CreateSolution();
+			sum += s.TimeTest(set[i]);
+		}
+		return sum;
+	}
+	double CorrectnessTest()
+	{
+		double sum = 0.0;
+		for (int i = 0; i < set.size(); i++)
+		{
+			Solution s = CreateSolution();
+			sum += s.CorrectnessTest(set[i]);
+		}
+		return sum / set.size();
+	}
 };
