ba448504b4150ff7fb5444fe9bb758acb5883d21
1657125180@qq.com <1657125180@qq.com>
2024-07-20 19:42:04 +0800

    chore:console output

--- Program/Program.h
+++ Program/Program.h
@@ -8,13 +8,13 @@
 #include"LiangBarskyAlgorithm.h"
 #include"CohenSutherlandAlgorithm.h"
 
-void Test(std::string path, int times,int printTimes, const std::function<Algorithm* ()>& CreateSolution)
+void Test(std::string path, int times, int printTimes, const std::function<Algorithm* ()>& CreateSolution, bool guaranteed = false)
 {
 	TestSerializer_Clip serializer;
 	TestSet set = TestSet(serializer.DeserializeFrom(path));
 	set.CreateSolution = CreateSolution;
 
-	set.AccuracyTest(printTimes);
+	set.AccuracyTest(printTimes, guaranteed);
 	set.TimeTest(times);
 
 	set.DeleteAnswers();
@@ -25,11 +25,11 @@ void Test(std::string path, int times,int printTimes, const std::function<Algori
 void TestLiangBarskyAlgorithm(std::string path, int times)
 {
 	std::cout << "LiangBarsky算法:" << std::endl;
-	Test(path, times, 0, LiangBarskyAlgorithm::CreateLiangBarskyAlgorithm);
+	Test(path, times, 3, LiangBarskyAlgorithm::CreateLiangBarskyAlgorithm, true);
 }
 
 void TestCohenSutherlandAlgorithm(std::string path, int times)
 {
 	std::cout << "CohenSutherland算法:" << std::endl;
-	Test(path, times, 0, CohenSutherlandAlgorithm::CreateCohenSutherlandAlgorithm);
+	Test(path, times, 0, CohenSutherlandAlgorithm::CreateCohenSutherlandAlgorithm, true);
 }
